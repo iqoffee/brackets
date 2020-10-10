481919142c824d3091fd6a5bf46daad852ecdc5a
iqoffee <iqoffee@users.noreply.github.com>
2020-10-10 11:17:42 +0500

    feat: check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let copyStr = str;
+
+    while (true) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            const firstPartRegExp = isNaN(parseInt(bracketsConfig[i][0]))
+                ? "\\" + bracketsConfig[i][0]
+                : bracketsConfig[i][0];
+            const secondPartRegExp = isNaN(parseInt(bracketsConfig[i][1]))
+                ? "\\" + bracketsConfig[i][1]
+                : bracketsConfig[i][1];
+
+            const regexp = new RegExp(firstPartRegExp + secondPartRegExp);
+
+            str = str.replace(regexp, "");
+        }
+
+        if (str === copyStr) {
+            return false;
+        }
+
+        if (str === "") {
+            return true;
+        }
+
+        copyStr = str;
+    }
+};
